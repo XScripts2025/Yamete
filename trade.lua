46a7a8960e7311a769498fb1de406638b96451e1
XScripts2025 <yametehahaku123@gmail.com>
2025-08-21 17:33:27 +0800

    Delete trade.lua

--- trade.lua
+++
@@ -1,8 +0,0 @@
---pet_high = "Frost Dragon"
---function RemovePet()
-
-while task.wait(0.1) do
-require(game.ReplicatedStorage.Fsys).load("RouterClient").get("TradeAPI/AcceptOrDeclineTradeRequest"):InvokeServer(true)
-    require(game.ReplicatedStorage.Fsys).load("RouterClient").get("TradeAPI/AcceptNegotiation"):FireServer()
-    require(game.ReplicatedStorage.Fsys).load("RouterClient").get("TradeAPI/ConfirmTrade"):FireServer()
-end
